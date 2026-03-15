536b3369422824ebcc5543b0dbfaeec8844b7add
Smarika-tech <smarikapokhrel96@gmail.com>
2026-03-15 18:41:30 +0545

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500 font">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
