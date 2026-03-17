c87e985548beb17d11790d8f212d7186815e692d
Smarika-tech <smarikapokhrel96@gmail.com>
2026-03-17 15:52:35 +0545

    feat:setup redux store and cart slice for state management

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,6 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "../store/cartSlice"
 
 export const store= configureStore({
-    
-}
-)
+    reducer: {
+        cart: cartReducer
+    }
+})
