a7f848c21c0801c449fc60480d022d12d693c591
Smarika-tech <smarikapokhrel96@gmail.com>
2026-03-21 23:28:05 +0545

    feat: implement CRUD operations

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,17 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState= {
-    items:[]
+    items:[] //but this state is a global state that can be accessed within the app
+    //api lai both state or store ma rakhna milxa but kaha use garney case ma farak parxa
 }
+
+//slice of the store
 
 export const cartSlice= createSlice({
   name:"cart",
   initialState,
   reducers: {
-    addToCart:()=>{},
+    addToCart:(state,action)=>{
+      const existing = state.items.find((item)=>item.id === action.payload.id)
+      if(existing){
+       existing.quantity += 1
+      }
+      else{
+      state.items.push({...action.payload, quantity:1})
+      }
+    },
+    increaseQty:(state,action)=>{
+        const item= state.items.find((item)=>item.id=== action.payload)
+          if(item)
+            item.quantity +=1
+        },
+     decreaseQty: (state, action) => {
+      const item = state.items.find(item => item.id === action.payload)
+      if (item) {
+        if (item.quantity === 1) {
+          state.items = state.items.filter(item => item.id !== action.payload)
+        } else {
+          item.quantity -= 1
+        }
+      }
+    }, 
+    removeFromCart:(state,action)=>{
+     state.items = state.items.filter(item => item.id !== action.payload)
+    }  
+    },
   }
-})
+)
 
-export const {addToCart}= cartSlice.actions;
+export const {addToCart,increaseQty,decreaseQty,removeFromCart}= cartSlice.actions;
 
 export default cartSlice.reducer;
